cd80cc1c1d5aba5ba5ce2b98797b437e5778b57d
jeswanth881 <70259677+jeswanth881@users.noreply.github.com>
2021-12-30 06:59:58 +0530

    Delete JwelleryListingApp directory

--- JwelleryListingApp/src/main/java/com/torryharris/JwelleryListingApp/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.torryharris.JwelleryListingApp.repository;
-
-import com.torryharris.JwelleryListingApp.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
